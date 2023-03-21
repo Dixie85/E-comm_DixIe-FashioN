24d4c27ea005727a362e6f0d806afa5025715f49
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-03-21 14:20:48 +0100

    mongoose create Users model

--- server/src/interfaces/interfaces.ts
+++ server/src/interfaces/interfaces.ts
@@ -1,4 +1,11 @@
 export interface IStatusError extends Error {
-    status?: number
-  }
-  
+  status?: number;
+}
+
+//User Interface
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  roles: string;
+}
---
+++ server/src/models/Users.ts
@@ -0,0 +1,25 @@
+import { IUser } from "../interfaces/interfaces";
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "User",
+    },
+  ],
+});
+
+export const Users = model<IUser>("user", userSchema);
