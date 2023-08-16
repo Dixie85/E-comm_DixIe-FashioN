8cd7c7e8dce2459a0f2b0d08d41ca2d887ba7594
Dixie85 <d_munja@hotmail.com>
2023-08-17 00:06:41 +0200

    ScrollUpBtn updated

--- client/src/Components/Buttons/ScrollUpBtn.tsx
+++ client/src/Components/Buttons/ScrollUpBtn.tsx
@@ -6,15 +6,19 @@ interface IScrollUpBtn {
 
 const ScrollUpBtn = ({ open }: IScrollUpBtn) => {
   return (
-    <section className={`fixed bottom-2 duration-500 ${!open ? 'right-2' : "-right-16" }`}>
+    <section className={`fixed flex items-center pr-2 py-0.5 pl-0.5 bg-rose-300/40 bottom-2 rounded-bl-xl rounded-tl-xl duration-500 z-50 ${!open ? 'right-0' : "-right-16"}`}>
       <button
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })}
         className='text-4xl group/arrow'>
         <span className='hidden group-hover/arrow:block'>
-          <IonIcon name='arrow-up-circle-sharp' />
+          <span className='flex items-center'>
+            <IonIcon name='arrow-up-circle-sharp' />
+          </span>
         </span>
         <span className='group-hover/arrow:hidden'>
-          <IonIcon name='arrow-up-circle-outline' />
+          <span className='flex items-center'>
+            <IonIcon name='arrow-up-circle-outline' />
+          </span>
         </span>
       </button>
     </section>
