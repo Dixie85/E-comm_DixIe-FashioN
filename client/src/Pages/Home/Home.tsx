4b0e9a356a7c64bcff1b2e646f4f23da096c5834
Dixie85 <d_munja@hotmail.com>
2023-08-17 14:51:47 +0200

    Home cleaned up

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,33 +1,25 @@
 import { useEffect } from 'react';
 import HeroSpiner from '../../Assets/Spiners/HeroSpiner';
 import Card from '../../Components/Card/Card'
-import FocusOnMe from '../../Components/FocusOnMe/FocusOnMe';
 import { IProduct } from '../../Interfaces/Interfaces';
 import { selectAllProducts } from '../../features/products/productsApiSlice';
 import { useAppSelector } from '../../redux/redux.hooks';
 
 const Home = () => {
-  // const products = useAppSelector(({ products }) => products.products);
   const products = useAppSelector(selectAllProducts) as IProduct[]
   
   useEffect(() => {
     window.scrollTo({ top: 0 });
   }, [])
   
-  //replace with spiner
   if (products.length < 1) return <HeroSpiner />
 
   const sales = products.filter(pro => pro.sale).slice(1, 5)
 
-
   return (
     <section className='max-w-[1440px] m-auto'>
-      {/* <FocusOnMe /> */}
       <article className="relative h-screen bg-Hero bg-cover
       font-[Josefin] md:bg-top bg-center">
-        {/* <h2 className="text-white text-2xl font-medium " >Discover the new you</h2>
-        <h1 className="md:text-5xl text-3xl text-white font-semibold py-5" ><span className='text-red-500'>DixIe</span> FashioN</h1> */}
-
         <h1 className='absolute top-56 right-10 lg:right-48 text-center' >
           <span className="text-white text-2xl font-medium " >discover the new you</span>
           <div className="md:text-6xl text-3xl text-white font-semibold my-3">
